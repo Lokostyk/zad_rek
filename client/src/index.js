aabdf7a79554ab7423cdb39fb1663edd17b91db0
Lokostyk <lokostyk12@gmail.com>
2022-04-10 23:33:42 +0200

    Added database link

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import axios from 'axios';
-axios.defaults.baseURL = ""
+axios.defaults.baseURL = "https://zad-rek-lokostyk.vercel.app/"
 
 const container = document.getElementById("root");
 const root = createRoot(container);
